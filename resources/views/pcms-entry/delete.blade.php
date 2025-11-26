b963194dd10b89134b10c5a0a3c70b9b0065cb9c
Eugen <eucas@my.cspc.edu.ph>
2025-11-26 08:14:53 +0800

    all

--- resources/views/pcms-entry/delete.blade.php
+++ resources/views/pcms-entry/delete.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
 
-    <h3><p>Are you sure to delete this entry "{{$entryList -> purpose}}?"</p></h3>
-    <a href="{{url('/entries/'.$entryList[0] -> entry_id.'/destroy')}}" style="text-decoration: none;">Yes</a>
+    <h3><p>Are you sure to delete this entry "{{$entry -> purpose}}?"</p></h3>
+    <a href="{{url('/entries/'.$entry -> entry_id.'/destroy')}}" style="text-decoration: none;">Yes</a>
     <a href="{{url('/entries')}}" style="text-decoration: none;">No</a>
 
 </x-layout>
--- resources/views/pcms-user/delete.blade.php
+++ resources/views/pcms-user/delete.blade.php
@@ -5,29 +5,29 @@
     </button>
 
     <div class="modal fade" id="deleteUserModal" tabindex="-1" aria-labelledby="deleteUserModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
+        <div class="modal-dialog">
+            <div class="modal-content">
 
-            <div class="modal-header">
-                <h5 class="modal-title" id="deleteUserModalLabel">Confirm Delete</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                <div class="modal-header">
+                    <h5 class="modal-title" id="deleteUserModalLabel">Confirm Delete</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                </div>
+
+                <div class="modal-body">
+                    Are you sure you want to delete the user:
+                    <strong>{{ $userList[0]->name }}</strong> ?
+                </div>
+
+                <div class="modal-footer">
+                    <a href="{{ url('/users/' . $userList[0]->user_id . '/destroy') }}" class="btn btn-danger">
+                        Yes, Delete
+                    </a>
+
+                    <a href="{{ url('/users') }}" class="btn btn-secondary">
+                        Cancel
+                    </a>
+                </div>
             </div>
-
-            <div class="modal-body">
-                Are you sure you want to delete the user:
-                <strong>{{ $userList[0]->name }}</strong> ?
-            </div>
-
-            <div class="modal-footer">
-                <a href="{{ url('/users/' . $userList[0]->user_id . '/destroy') }}" class="btn btn-danger">
-                    Yes, Delete
-                </a>
-
-                <a href="{{ url('/users') }}" class="btn btn-secondary">
-                    Cancel
-                </a>
-            </div>
-
         </div>
     </div>
 
